04d17d52c30f59afe99e1cd10a2f45dd979a14cd
charles.wangkai <charles.wangkai@19f3f850-74e3-513f-8ccf-407a04c0de7c>
2012-08-25 03:21:54 +0000

    greasemonkey js
    git-svn-id: https://goalboytopcoder.googlecode.com/svn/trunk@10 19f3f850-74e3-513f-8ccf-407a04c0de7c

--- greasemonkey/topcoder_solved_problem_marker.user.js
+++ greasemonkey/topcoder_solved_problem_marker.user.js
@@ -7,23 +7,25 @@
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js
 // ==/UserScript==
 
-var markSolvedProblem = function($problemName) {
-	var url = 'http://goalboytopcoder.googlecode.com/svn/trunk/' + $problemName.html().trim() + '.java';
-	GM_xmlhttpRequest({
-		method: 'GET',
-		url: url,
-		onload: function(resp) {
-			if (resp.status === 200) {
-				$problemName.css('background-color', 'green');
+$(document).ready(function() {
+	var markSolvedProblem = function($problemName) {
+		var url = 'http://goalboytopcoder.googlecode.com/svn/trunk/' + $problemName.html().trim() + '.java';
+		GM_xmlhttpRequest({
+			method: 'GET',
+			url: url,
+			onload: function(resp) {
+				if (resp.status === 200) {
+					$problemName.css('background-color', 'green');
+				}
 			}
-		}
+		});
+	};
+	
+	var $problemNames = $('a.statText').filter(function() {
+		return $(this).attr('href').indexOf('/stat?c=problem_statement&pm=') >= 0;
 	});
-};
-
-var $problemNames = $('a.statText').filter(function() {
-	return $(this).attr('href').indexOf('/stat?c=problem_statement&pm=') >= 0;
-});
-
-$.each($problemNames, function(index, value) {
-	markSolvedProblem($(value));
+	
+	$.each($problemNames, function(index, value) {
+		markSolvedProblem($(value));
+	});
 });
