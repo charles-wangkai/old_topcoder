782ebf0e122e4d9e34793a0ebbf6230d31146e81
charles.wangkai <charles.wangkai@19f3f850-74e3-513f-8ccf-407a04c0de7c>
2012-09-08 04:00:06 +0000

    CCipher
    git-svn-id: https://goalboytopcoder.googlecode.com/svn/trunk@33 19f3f850-74e3-513f-8ccf-407a04c0de7c

--- CCipher.java
+++ CCipher.java
@@ -1,8 +1,8 @@
 public class CCipher {
 	public String decode(String cipherText, int shift) {
-		String result="";
-		for (int i=0;i<cipherText.length();i++) {
-			result+=(char)((cipherText.charAt(i)-'A'-shift+26)%26+'A');
+		String result = "";
+		for (int i = 0; i < cipherText.length(); i++) {
+			result += (char) ((cipherText.charAt(i) - 'A' - shift + 26) % 26 + 'A');
 		}
 		return result;
 	}
