d6ff925cbeb17ca864c07a6ba2bb24e1a469c86d
charles.wangkai <charles.wangkai@19f3f850-74e3-513f-8ccf-407a04c0de7c>
2012-09-10 18:36:02 +0000

    CrossWord
    git-svn-id: https://goalboytopcoder.googlecode.com/svn/trunk@88 19f3f850-74e3-513f-8ccf-407a04c0de7c

---
+++ CrossWord.java
@@ -0,0 +1,19 @@
+public class CrossWord {
+	public int countWords(String[] board, int size) {
+		int slotNum = 0;
+		for (String row : board) {
+			int slotLength = 0;
+			for (int i = 0; i <= row.length(); i++) {
+				if (i < row.length() && row.charAt(i) == '.') {
+					slotLength++;
+				} else {
+					if (slotLength == size) {
+						slotNum++;
+					}
+					slotLength = 0;
+				}
+			}
+		}
+		return slotNum;
+	}
+}
