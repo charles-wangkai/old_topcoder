90f441fb9da48bb8aee0ea49e96903c98eaddf6d
charles.wangkai <charles.wangkai@19f3f850-74e3-513f-8ccf-407a04c0de7c>
2012-09-12 06:51:32 +0000

    ImageDithering
    git-svn-id: https://goalboytopcoder.googlecode.com/svn/trunk@113 19f3f850-74e3-513f-8ccf-407a04c0de7c

--- ImageDithering.java
+++ ImageDithering.java
@@ -1,17 +1,13 @@
 public class ImageDithering {
 	public int count(String dithered, String[] screen) {
-		boolean dither[] = new boolean[26];
-		for (int i = 0; i < dithered.length(); i++) {
-			dither[dithered.charAt(i) - 'A'] = true;
-		}
-		int count = 0;
-		for (int i = 0; i < screen.length; i++) {
-			for (int j = 0; j < screen[i].length(); j++) {
-				if (dither[screen[i].charAt(j) - 'A']) {
-					count++;
+		int ditherNum = 0;
+		for (String row : screen) {
+			for (int i = 0; i < row.length(); i++) {
+				if (dithered.indexOf(row.charAt(i)) >= 0) {
+					ditherNum++;
 				}
 			}
 		}
-		return count;
+		return ditherNum;
 	}
 }
