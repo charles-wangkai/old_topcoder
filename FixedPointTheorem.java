e583750c6d674dc1d218c28b6cb2a21025f3e7ac
charles.wangkai <charles.wangkai@19f3f850-74e3-513f-8ccf-407a04c0de7c>
2012-09-14 10:03:55 +0000

    FixedPointTheorem
    git-svn-id: https://goalboytopcoder.googlecode.com/svn/trunk@136 19f3f850-74e3-513f-8ccf-407a04c0de7c

---
+++ FixedPointTheorem.java
@@ -0,0 +1,15 @@
+public class FixedPointTheorem {
+	public double cycleRange(double R) {
+		double min = Double.MAX_VALUE;
+		double max = Double.MIN_VALUE;
+		double x = 0.25;
+		for (int i = 0; i < 201000; i++) {
+			x = R * x * (1 - x);
+			if (i >= 200000) {
+				min = Math.min(min, x);
+				max = Math.max(max, x);
+			}
+		}
+		return max - min;
+	}
+}
