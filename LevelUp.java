3e8d60114fd9e4b24f46d923cbd2151ca6847037
charles.wangkai <charles.wangkai@19f3f850-74e3-513f-8ccf-407a04c0de7c>
2012-09-17 13:08:37 +0000

    LevelUp
    git-svn-id: https://goalboytopcoder.googlecode.com/svn/trunk@169 19f3f850-74e3-513f-8ccf-407a04c0de7c

---
+++ LevelUp.java
@@ -0,0 +1,9 @@
+public class LevelUp {
+	public int toNextLevel(int[] expNeeded, int received) {
+		for (int i = 0;; i++) {
+			if (expNeeded[i] > received) {
+				return expNeeded[i] - received;
+			}
+		}
+	}
+}
