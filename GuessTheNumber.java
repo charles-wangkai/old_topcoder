f860f3201e099cc0ea751f00ed2390db1d10d434
charles.wangkai <charles.wangkai@19f3f850-74e3-513f-8ccf-407a04c0de7c>
2012-09-17 03:22:02 +0000

    GuessTheNumber
    git-svn-id: https://goalboytopcoder.googlecode.com/svn/trunk@156 19f3f850-74e3-513f-8ccf-407a04c0de7c

---
+++ GuessTheNumber.java
@@ -0,0 +1,16 @@
+public class GuessTheNumber {
+	public int noGuesses(int upper, int answer) {
+		int lower = 1;
+		for (int i = 1;; i++) {
+			int x = (lower + upper) / 2;
+			if (x == answer) {
+				return i;
+			}
+			if (x < answer) {
+				lower = x + 1;
+			} else {
+				upper = x - 1;
+			}
+		}
+	}
+}
