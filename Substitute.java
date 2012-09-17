488d9e9c4675e239bd2d0687d4b3e9d47b10c10f
charles.wangkai <charles.wangkai@19f3f850-74e3-513f-8ccf-407a04c0de7c>
2012-09-17 06:10:44 +0000

    Substitute
    git-svn-id: https://goalboytopcoder.googlecode.com/svn/trunk@162 19f3f850-74e3-513f-8ccf-407a04c0de7c

---
+++ Substitute.java
@@ -0,0 +1,12 @@
+public class Substitute {
+	public int getValue(String key, String code) {
+		int number = 0;
+		for (int i = 0; i < code.length(); i++) {
+			int index = key.indexOf(code.charAt(i));
+			if (index >= 0) {
+				number = number * 10 + (index + 1) % 10;
+			}
+		}
+		return number;
+	}
+}
