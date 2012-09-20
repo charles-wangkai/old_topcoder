273ebcc73c7c9f5cfbda81de7fe38ad504cbe24f
charles.wangkai <charles.wangkai@19f3f850-74e3-513f-8ccf-407a04c0de7c>
2012-09-20 16:55:44 +0000

    Multiples
    git-svn-id: https://goalboytopcoder.googlecode.com/svn/trunk@229 19f3f850-74e3-513f-8ccf-407a04c0de7c

---
+++ Multiples.java
@@ -0,0 +1,16 @@
+public class Multiples {
+	public int number(int min, int max, int factor) {
+		int minMultiple = min / factor * factor;
+		if (minMultiple < min) {
+			minMultiple += factor;
+		}
+		if (minMultiple > max) {
+			return 0;
+		}
+		int maxMultiple = max / factor * factor;
+		if (maxMultiple > max) {
+			maxMultiple -= factor;
+		}
+		return (maxMultiple - minMultiple) / factor + 1;
+	}
+}
