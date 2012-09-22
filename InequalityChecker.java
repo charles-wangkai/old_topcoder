787eb3a855fd46251774030d7b8ef9bee70ff9f0
charles.wangkai <charles.wangkai@19f3f850-74e3-513f-8ccf-407a04c0de7c>
2012-09-22 18:14:44 +0000

    InequalityChecker
    git-svn-id: https://goalboytopcoder.googlecode.com/svn/trunk@254 19f3f850-74e3-513f-8ccf-407a04c0de7c

---
+++ InequalityChecker.java
@@ -0,0 +1,15 @@
+public class InequalityChecker {
+	public int[] getDifferences(int n) {
+		int numerator = 2 * n * n * n - n * n * n * n;
+		for (int i = 1; i <= n - 1; i++) {
+			numerator += 4 * i * i * i;
+		}
+		if (numerator % 4 == 0) {
+			return new int[] { numerator / 4, 1 };
+		} else if (numerator % 2 == 0) {
+			return new int[] { numerator / 2, 2 };
+		} else {
+			return new int[] { numerator, 4 };
+		}
+	}
+}
