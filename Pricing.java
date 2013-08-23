e3d45c07757d9196e34768bd366cfc4303f06717
charles.wangkai <charles.wangkai@19f3f850-74e3-513f-8ccf-407a04c0de7c>
2013-08-23 06:22:57 +0000

    Pricing
    git-svn-id: https://goalboytopcoder.googlecode.com/svn/trunk@727 19f3f850-74e3-513f-8ccf-407a04c0de7c

--- Pricing.java
+++ Pricing.java
@@ -4,32 +4,40 @@ import java.util.Arrays;
 public class Pricing {
 	public int maxSales(int[] price) {
 		Arrays.sort(price);
-		ArrayList<Price> prices=new ArrayList<Price>();
-		int previous=-1;
-		for (int i=0;i<price.length;i++) {
-			if (price[i]!=previous) {
-				prices.add(new Price(previous,i-1));
-				previous=price[i];
+		ArrayList<Price> prices = new ArrayList<Price>();
+		int previous = -1;
+		for (int i = 0; i < price.length; i++) {
+			if (price[i] != previous) {
+				prices.add(new Price(previous, i - 1));
+				previous = price[i];
 			}
 		}
-		prices.add(new Price(previous,price.length-1));
-		int max=0;
-		if (prices.size()<=4) {
-			for (int i=0;i<price.length;i++) {
-				max+=price[i];
+		prices.add(new Price(previous, price.length - 1));
+		int max = 0;
+		if (prices.size() <= 4) {
+			for (int i = 0; i < price.length; i++) {
+				max += price[i];
 			}
-		}
-		else {
-			for (int i=1;i<prices.size();i++) {
-				for (int j=i+1;j<prices.size();j++) {
-					int partMax1=prices.get(i).value*(prices.get(j-1).accumulate-prices.get(i-1).accumulate);
-					for (int k=j+1;k<prices.size();k++) {
-						int partMax2=partMax1+prices.get(j).value*(prices.get(k-1).accumulate-prices.get(j-1).accumulate);
-						for (int p=k+1;p<prices.size();p++) {
-							int partMax3=partMax2+prices.get(k).value*(prices.get(p-1).accumulate-prices.get(k-1).accumulate);
-							int partMax4=partMax3+prices.get(p).value*(price.length-1-prices.get(p-1).accumulate);
-							if (partMax4>max) {
-								max=partMax4;
+		} else {
+			for (int i = 1; i < prices.size(); i++) {
+				for (int j = i + 1; j < prices.size(); j++) {
+					int partMax1 = prices.get(i).value
+							* (prices.get(j - 1).accumulate - prices.get(i - 1).accumulate);
+					for (int k = j + 1; k < prices.size(); k++) {
+						int partMax2 = partMax1
+								+ prices.get(j).value
+								* (prices.get(k - 1).accumulate - prices
+										.get(j - 1).accumulate);
+						for (int p = k + 1; p < prices.size(); p++) {
+							int partMax3 = partMax2
+									+ prices.get(k).value
+									* (prices.get(p - 1).accumulate - prices
+											.get(k - 1).accumulate);
+							int partMax4 = partMax3
+									+ prices.get(p).value
+									* (price.length - 1 - prices.get(p - 1).accumulate);
+							if (partMax4 > max) {
+								max = partMax4;
 							}
 						}
 					}
@@ -38,18 +46,14 @@ public class Pricing {
 		}
 		return max;
 	}
-	
-	public static void main(String args[]) {
-		Pricing p=new Pricing();
-		System.out.println(p.maxSales(new int[]{9,1,5,5,5,5,4,8,80}));
-	}
 }
 
 class Price {
 	int value;
 	int accumulate;
-	Price(int theValue,int theAccumulate) {
-		this.value=theValue;
-		this.accumulate=theAccumulate;
+
+	Price(int theValue, int theAccumulate) {
+		this.value = theValue;
+		this.accumulate = theAccumulate;
 	}
 }
