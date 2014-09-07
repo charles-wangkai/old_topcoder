ef50da0a246095f90aaca78ed461d689d2c3771e
charles.wangkai <charles.wangkai@gmail.com>
2014-09-07 09:58:59 +0800

    FoxAndGame

--- FoxAndGame.java
+++ FoxAndGame.java
@@ -2,8 +2,11 @@ import java.util.Arrays;
 
 public class FoxAndGame {
 	public int countStars(String[] result) {
-		return Arrays.asList(result).stream()
-				.map((stage) -> stage.chars().filter(c -> c == 'o').count())
-				.reduce((t, u) -> t + u).get().intValue();
+		return (int) Arrays
+				.asList(result)
+				.stream()
+				.mapToLong(
+						(stage) -> stage.chars().filter(c -> c == 'o').count())
+				.sum();
 	}
 }
